2f27385a41d46f11bfd3bfa2b91df31f29758d17
wootguy <drake.rhunter@gmail.com>
2020-12-05 04:39:15 -0800

    cleanup/update template code

--- dlls/monster/tempmonster.cpp
+++ dlls/monster/tempmonster.cpp
@@ -17,45 +17,64 @@
 //=========================================================
 #if 0
 
-#include	"extdll.h"
-#include	"util.h"
-#include	"cbase.h"
-#include	"monsters.h"
-#include	"schedule.h"
-
-//=========================================================
-// Monster's Anim Events Go Here
-//=========================================================
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "monsters.h"
+#include "schedule.h"
 
 class CMyMonster : public CBaseMonster
 {
 public:
 	void Spawn( void );
 	void Precache( void );
+	int Classify(void);
 	void SetYawSpeed( void );
-	int  Classify ( void );
 	void HandleAnimEvent( MonsterEvent_t *pEvent );
+
+	BOOL CheckRangeAttack1(float flDot, float flDist) { return FALSE; }
+	BOOL CheckRangeAttack2(float flDot, float flDist) { return FALSE; }
+	BOOL CheckMeleeAttack1(float flDot, float flDist);
 };
+
 LINK_ENTITY_TO_CLASS( my_monster, CMyMonster );
 
-//=========================================================
-// Classify - indicates this monster's place in the 
-// relationship table.
-//=========================================================
-int	CMyMonster :: Classify ( void )
+void CMyMonster :: Spawn()
 {
-	return	CLASS_MY_MONSTER;
+	Precache( );
+
+	SET_MODEL(ENT(pev), "models/mymodel.mdl");
+	UTIL_SetSize( pev, Vector( -16, -16, 0 ), Vector( 16, 16, 32 ) );
+
+	pev->solid = SOLID_SLIDEBOX;
+	pev->movetype = MOVETYPE_STEP;
+	m_bloodColor = BLOOD_COLOR_GREEN;
+	pev->health = 100;
+	pev->view_ofs = Vector ( 0, 0, 0 );
+	m_flFieldOfView = 0.0;
+	m_MonsterState = MONSTERSTATE_NONE;
+	m_afCapability = bits_CAP_MELEE_ATTACK1;
+
+	MonsterInit();
 }
 
-//=========================================================
-// SetYawSpeed - allows each sequence to have a different
-// turn rate associated with it.
-//=========================================================
-void CMyMonster :: SetYawSpeed ( void )
+void CMyMonster :: Precache()
+{
+	PRECACHE_SOUND("mysound.wav");
+
+	PRECACHE_MODEL("models/mymodel.mdl");
+}
+
+int	CMyMonster::Classify(void)
+{
+	return CLASS_ALIEN_MONSTER;
+}
+
+void CMyMonster::SetYawSpeed(void)
 {
 	int ys;
 
-	switch ( m_Activity )
+	switch (m_Activity)
 	{
 	case ACT_IDLE:
 	default:
@@ -65,53 +84,19 @@ void CMyMonster :: SetYawSpeed ( void )
 	pev->yaw_speed = ys;
 }
 
-//=========================================================
-// HandleAnimEvent - catches the monster-specific messages
-// that occur when tagged animation frames are played.
-//=========================================================
-void CMyMonster :: HandleAnimEvent( MonsterEvent_t *pEvent )
+void CMyMonster::HandleAnimEvent(MonsterEvent_t* pEvent)
 {
-	switch( pEvent->event )
+	switch (pEvent->event)
 	{
 	case 0:
 	default:
-		CBaseMonster::HandleAnimEvent( pEvent );
+		CBaseMonster::HandleAnimEvent(pEvent);
 		break;
 	}
 }
 
-//=========================================================
-// Spawn
-//=========================================================
-void CMyMonster :: Spawn()
-{
-	Precache( );
-
-	SET_MODEL(ENT(pev), "models/mymodel.mdl");
-	UTIL_SetSize( pev, Vector( -12, -12, 0 ), Vector( 12, 12, 24 ) );
-
-	pev->solid			= SOLID_SLIDEBOX;
-	pev->movetype		= MOVETYPE_STEP;
-	m_bloodColor		= BLOOD_COLOR_GREEN;
-	pev->health			= 8;
-	pev->view_ofs		= Vector ( 0, 0, 0 );// position of the eyes relative to monster's origin.
-	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
-	m_MonsterState		= MONSTERSTATE_NONE;
-
-	MonsterInit();
+BOOL CMyMonster::CheckMeleeAttack1(float flDot, float flDist) {
+	return flDist < 128 ? TRUE : FALSE;
 }
 
-//=========================================================
-// Precache - precaches all resources this monster needs
-//=========================================================
-void CMyMonster :: Precache()
-{
-	PRECACHE_SOUND("mysound.wav");
-
-	PRECACHE_MODEL("models/mymodel.mdl");
-}	
-
-//=========================================================
-// AI Schedules Specific to this monster
-//=========================================================
 #endif
