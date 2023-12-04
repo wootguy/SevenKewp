d8f7822bf86ab6e07db41dcc9910f5617c8fcdba
wootguy <w00tguy123@gmail.com>
2023-12-03 23:22:49 -0800

    better spawn point selection + disabled flag fix
    the disabled flag was ignored and old logic picked randomly only from the first 5 spawn points

--- dlls/CBaseDMStart.h
+++ dlls/CBaseDMStart.h
@@ -1,5 +1,5 @@
 edict_t* EntSelectSpawnPoint(CBaseEntity* pPlayer);
 
-BOOL IsSpawnPointValid(CBaseEntity* pPlayer, CBaseEntity* pSpot);
+BOOL IsSpawnPointClear(CBaseEntity* pPlayer, CBaseEntity* pSpot);
 
 inline int FNullEnt(CBaseEntity* ent) { return (ent == NULL) || FNullEnt(ent->edict()); }
