429a69dddf3235ba9d11f219b4b94c212eadb6fa
wootguy <w00tguy123@gmail.com>
2023-12-06 01:47:27 -0800

    implement timed mode for trigger_random
    entity is finished now. It handles conversion from trigger_random_unique and trigger_random_time.

--- dlls/triggers/CTriggerRandom.cpp
+++ dlls/triggers/CTriggerRandom.cpp
@@ -22,6 +22,9 @@ public:
 	void Spawn(void);
 	void KeyValue(KeyValueData* pkvd);
 	void Use(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value);
+	void EXPORT TimedThink();
+
+	string_t SelectRandomTarget();
 
 	string_t targets[MAX_RANDOM_TARGETS];
 	bool firedTargets[MAX_RANDOM_TARGETS];
@@ -34,6 +37,7 @@ public:
 
 LINK_ENTITY_TO_CLASS(trigger_random, CTriggerRandom);
 LINK_ENTITY_TO_CLASS(trigger_random_unique, CTriggerRandom);
+LINK_ENTITY_TO_CLASS(trigger_random_time, CTriggerRandom);
 
 void CTriggerRandom::KeyValue(KeyValueData* pkvd)
 {
@@ -45,13 +49,12 @@ void CTriggerRandom::KeyValue(KeyValueData* pkvd)
 	else if (FStrEq(pkvd->szKeyName, "min_delay"))
 	{
 		ALERT(at_console, "trigger_random: Timed mode not implemented\n");
-		min_delay = atof(pkvd->szValue);
+		min_delay = max(0, atof(pkvd->szValue));
 		pkvd->fHandled = TRUE;
 	}
 	else if (FStrEq(pkvd->szKeyName, "max_delay"))
 	{
-		
-		max_delay = atof(pkvd->szValue);
+		max_delay = max(0, atof(pkvd->szValue));
 		pkvd->fHandled = TRUE;
 	}
 	else {
@@ -87,51 +90,92 @@ void CTriggerRandom::Spawn(void)
 		}
 		pev->spawnflags |= SF_UNIQUE;
 	}
+
+	if (!strcmp(STRING(pev->classname), "trigger_random_time")) {
+		pev->spawnflags |= SF_TIMED;
+	}
 	
+	if ((pev->spawnflags & SF_TIMED) && (pev->spawnflags & SF_START_ON)) {
+		SetThink(&CTriggerRandom::TimedThink);
+		pev->nextthink = gpGlobals->time + RANDOM_FLOAT(min_delay, max_delay);
+	}
+
 	if (pev->spawnflags & (SF_START_ON | SF_TRIGGER_ONCE | SF_TIMED)) {
 		ALERT(at_error, "trigger_random: timed mode not implemented\n");
 	}
 }
 
+void CTriggerRandom::TimedThink() {
+	string_t target = SelectRandomTarget();
+
+	if (target) {
+		FireTargets(STRING(target), this, this, USE_TOGGLE, 0);
+	}
+
+	if (pev->spawnflags & SF_TRIGGER_ONCE) {
+		SetThink(NULL);
+		pev->nextthink = 0;
+		return;
+	}
+
+	pev->nextthink = gpGlobals->time + RANDOM_FLOAT(min_delay, max_delay);
+}
+
+string_t CTriggerRandom::SelectRandomTarget() {
+	int randomIdx;
+
+	if (pev->spawnflags & SF_UNIQUE) {
+		int remainingTargetCount = 0;
+		int remainingTargets[16];
+		for (int i = 0; i < target_count; i++) {
+			if (!firedTargets[i]) {
+				remainingTargets[remainingTargetCount++] = i;
+			}
+		}
+
+		if (remainingTargetCount == 0) {
+			if (pev->spawnflags & SF_REUSABLE) {
+				memset(firedTargets, 0, sizeof(bool) * MAX_RANDOM_TARGETS);
+				randomIdx = RANDOM_LONG(0, target_count - 1);
+			}
+			else {
+				return 0; // fired all targets and is not flagged as reusable
+			}
+		}
+		else {
+			randomIdx = remainingTargets[RANDOM_LONG(0, remainingTargetCount - 1)];
+		}
+	}
+	else {
+		randomIdx = RANDOM_LONG(0, target_count - 1);
+	}
+
+	string_t target = targets[randomIdx];
+	firedTargets[randomIdx] = true;
+
+	return target;
+}
 
 void CTriggerRandom::Use(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value)
 {
 	isActive = useType == USE_TOGGLE ? !isActive : useType;
 
-	if ((pev->spawnflags & SF_TIMED) == 0) {
-		int randomIdx;
-
-		if (pev->spawnflags & SF_UNIQUE) {
-			int remainingTargetCount = 0;
-			int remainingTargets[16];
-			for (int i = 0; i < target_count; i++) {
-				if (!firedTargets[i]) {
-					remainingTargets[remainingTargetCount++] = i;
-				}
-			}
-
-			if (remainingTargetCount == 0) {
-				if (pev->spawnflags & SF_REUSABLE) {
-					memset(firedTargets, 0, sizeof(bool) * MAX_RANDOM_TARGETS);
-					randomIdx = RANDOM_LONG(0, target_count - 1);
-				}
-				else {
-					return; // fired all targets and is not flagged as reusable
-				}
-			}
-			else {
-				randomIdx = remainingTargets[RANDOM_LONG(0, remainingTargetCount - 1)];
-			}
+	if (pev->spawnflags & SF_TIMED) {
+		ALERT(at_console, "random time %s triggered %d\n", STRING(pev->targetname), isActive);
+		if (isActive) {
+			SetThink(&CTriggerRandom::TimedThink);
+			pev->nextthink = gpGlobals->time + RANDOM_FLOAT(min_delay, max_delay);
 		}
 		else {
-			randomIdx = RANDOM_LONG(0, target_count - 1);
+			SetThink(NULL);
+			pev->nextthink = 0;
 		}
+		return;
+	}
 
-		string_t target = targets[randomIdx];
-		firedTargets[randomIdx] = true;
+	string_t target = SelectRandomTarget();
 
-		if (target) {
-			FireTargets(STRING(target), pActivator, this, useType, value);
-		}
+	if (target) {
+		FireTargets(STRING(target), pActivator, this, USE_TOGGLE, 0);
 	}
 }
