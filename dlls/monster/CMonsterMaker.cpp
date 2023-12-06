df630e51780a3089b5b94a327df89ff1f5149618
wootguy <w00tguy123@gmail.com>
2023-12-06 13:21:27 -0800

    add env_xenmaker and "Force spawn" mode
    xenmaker effects in sven use a custom message that isn't available to vanilla HL clients, so this uses a bunch of default TE messages instead (might need to limit the beam count to something lower later).

--- dlls/monster/CMonsterMaker.cpp
+++ dlls/monster/CMonsterMaker.cpp
@@ -28,6 +28,16 @@
 #define	SF_MONSTERMAKER_CYCLIC		4 // drop one monster every time fired.
 #define SF_MONSTERMAKER_MONSTERCLIP	8 // Children are blocked by monsterclip
 
+#define MAX_XENMAKER_BEAMS 64 // each beam will need a TE message so don't go too crazy...
+#define XENMAKER_SOUND1 "debris/beamstart7.wav"
+#define XENMAKER_SOUND2 "debris/beamstart2.wav"
+
+enum blocked_spawn_modes {
+	SPAWN_BLOCK_LEGACY, // fail the spawn if blocked
+	SPAWN_BLOCK_WAIT, // wait for the blockage to clear, then spawn
+	SPAWN_BLOCK_IGNORE // spawn even if blocked (xenmaker mode)
+};
+
 //=========================================================
 // MonsterMaker - this ent creates monsters during the game.
 //=========================================================
@@ -42,6 +52,7 @@ public:
 	void EXPORT MakerThink ( void );
 	void DeathNotice ( entvars_t *pevChild );// monster maker children use this to tell the monster maker that they have died.
 	void MakeMonster( void );
+	void XenmakerEffect();
 
 	virtual int		Save( CSave &save );
 	virtual int		Restore( CRestore &restore );
@@ -60,10 +71,29 @@ public:
 
 	BOOL m_fActive;
 	BOOL m_fFadeChildren;// should we make the children fadeout?
+
+	int m_blockedSpawnMode;
+
+	// env_xenmaker effect settings
+	float m_flBeamRadius;
+	int m_iBeamAlpha;
+	int m_iBeamCount;
+	Vector m_vBeamColor;
+	float m_flLightRadius;
+	Vector m_vLightColor;
+	float m_flStartSpriteFramerate;
+	float m_flStartSpriteScale;
+	int m_iStartSpriteAlpha;
+	int m_xenSpriteIdx;
+	int m_xenBeamSpriteIdx;
+	float m_nextXenSound;
+
+	string_t m_xenmakerTemplate; // grab xenmaker settings from another entity
 };
 
 LINK_ENTITY_TO_CLASS( monstermaker, CMonsterMaker );
 LINK_ENTITY_TO_CLASS( squadmaker, CMonsterMaker );
+LINK_ENTITY_TO_CLASS( env_xenmaker, CMonsterMaker );
 
 TYPEDESCRIPTION	CMonsterMaker::m_SaveData[] = 
 {
@@ -83,7 +113,6 @@ IMPLEMENT_SAVERESTORE( CMonsterMaker, CBaseMonster );
 
 void CMonsterMaker :: KeyValue( KeyValueData *pkvd )
 {
-	
 	if ( FStrEq(pkvd->szKeyName, "monstercount") )
 	{
 		m_cNumMonsters = atoi(pkvd->szValue);
@@ -104,6 +133,101 @@ void CMonsterMaker :: KeyValue( KeyValueData *pkvd )
 		pev->model = ALLOC_STRING(pkvd->szValue);
 		pkvd->fHandled = TRUE;
 	}
+	else if (FStrEq(pkvd->szKeyName, "spawn_mode"))
+	{
+		m_blockedSpawnMode = atoi(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "xenmaker"))
+	{
+		m_xenmakerTemplate = ALLOC_STRING(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+
+	// env_xenmaker keys
+	else if (FStrEq(pkvd->szKeyName, "m_flBeamRadius"))
+	{
+		m_flBeamRadius = atof(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_iBeamAlpha"))
+	{
+		m_iBeamAlpha = atoi(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_iBeamCount"))
+	{
+		m_iBeamCount = atoi(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_vBeamColor"))
+	{
+		UTIL_StringToVector(m_vBeamColor, pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_flLightRadius"))
+	{
+		m_flLightRadius = atof(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_vLightColor"))
+	{
+		UTIL_StringToVector(m_vLightColor, pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_flStartSpriteFramerate"))
+	{
+		m_flStartSpriteFramerate = atof(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_flStartSpriteScale"))
+	{
+		m_flStartSpriteScale = atof(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_iStartSpriteAlpha"))
+	{
+		// svencoop.fgd implies 255 is fully opaque but it's actually inverted.
+		// 255 = barely visible. 1 = max opaqueness. 0 = invisible.
+		// most (all?) maps use the default 255 so you may not have ever seen this sprite!
+		// This should maybe treat 255 as invisible to save a precache slot.
+		m_iStartSpriteAlpha = abs(atoi(pkvd->szValue)) % 256;
+		if (m_iStartSpriteAlpha > 0) {
+			m_iStartSpriteAlpha = 255 - (m_iStartSpriteAlpha-1);
+		}
+
+		pkvd->fHandled = TRUE;
+	}
+
+	// the following xenmaker keys exist but have no effect.
+	// There is no "EndSprite" and sprite color can't be changed.
+	/*
+	else if (FStrEq(pkvd->szKeyName, "m_vStartpriteColor"))
+	{
+		UTIL_StringToVector(m_vStartpriteColor, pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_flEndSpriteFramerate"))
+	{
+		m_flEndSpriteFramerate= atof(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_flEndSpriteScale"))
+	{
+		m_flEndSpriteScale = atof(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_iEndSpriteAlpha"))
+	{
+		m_iEndSpriteAlpha= atoi(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else if (FStrEq(pkvd->szKeyName, "m_vEndSpriteColor"))
+	{
+		UTIL_StringToVector(m_vEndSpriteColor, pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	*/
 
 	// TODO: ripent the maps or something. This key logic is duplicated in CBaseMonster
 	else if (FStrEq(pkvd->szKeyName, "trigger_target"))
@@ -133,6 +257,21 @@ void CMonsterMaker :: Spawn( )
 
 	m_cLiveChildren = 0;
 	Precache();
+	
+	if (!strcmp(STRING(pev->classname), "env_xenmaker")) {
+		if (pev->spawnflags & 1) { // "Try Once"
+			// according to sven manor this should change how blocked spawning works, but it does nothing
+			// xenmaker always forces a spawn, even if blocked
+		}
+		if (pev->spawnflags & 2) { // "No Spawn"
+			// hack to prevent anything from spawning
+			m_cLiveChildren = m_iMaxLiveChildren = 1;
+		}
+		
+		pev->spawnflags = SF_MONSTERMAKER_CYCLIC;
+		m_blockedSpawnMode = SPAWN_BLOCK_IGNORE;
+	}
+
 	if ( !FStringNull ( pev->targetname ) )
 	{
 		if ( pev->spawnflags & SF_MONSTERMAKER_CYCLIC )
@@ -183,6 +322,13 @@ void CMonsterMaker :: Precache( void )
 	if (pev->model) {
 		PRECACHE_MODEL(STRING(pev->model));
 	}
+
+	if (!strcmp(STRING(pev->classname), "env_xenmaker")) {
+		m_xenSpriteIdx = PRECACHE_MODEL("sprites/fexplo1.spr");
+		m_xenBeamSpriteIdx = PRECACHE_MODEL("sprites/laserbeam.spr");
+		PRECACHE_SOUND(XENMAKER_SOUND1);
+		PRECACHE_SOUND(XENMAKER_SOUND2);
+	}
 }
 
 //=========================================================
@@ -192,6 +338,8 @@ void CMonsterMaker::MakeMonster( void )
 {
 	edict_t	*pent;
 	entvars_t		*pevCreate;
+
+	XenmakerEffect();
 
 	if ( m_iMaxLiveChildren > 0 && m_cLiveChildren >= m_iMaxLiveChildren )
 	{// not allowed to make a new one yet. Too many live ones out right now.
@@ -212,12 +360,18 @@ void CMonsterMaker::MakeMonster( void )
 	maxs.z = pev->origin.z;
 	mins.z = m_flGround;
 
-	CBaseEntity *pList[2];
-	int count = UTIL_EntitiesInBox( pList, 2, mins, maxs, FL_CLIENT|FL_MONSTER );
-	if ( count )
-	{
-		// don't build a stack of monsters!
-		return;
+	if (m_blockedSpawnMode != SPAWN_BLOCK_IGNORE) {
+		CBaseEntity* pList[2];
+		int count = UTIL_EntitiesInBox(pList, 2, mins, maxs, FL_CLIENT | FL_MONSTER);
+		if (count)
+		{
+			// don't build a stack of monsters!
+			if (m_blockedSpawnMode == SPAWN_BLOCK_WAIT) {
+				ALERT(at_console, "monstermaker: SPAWN_BLOCK_WAIT mode not implemented!");
+			} else {
+				return;
+			}
+		}
 	}
 
 	pent = CREATE_NAMED_ENTITY( m_iszMonsterClassname );
@@ -316,6 +470,102 @@ void CMonsterMaker :: ToggleUse ( CBaseEntity *pActivator, CBaseEntity *pCaller,
 	pev->nextthink = gpGlobals->time;
 }
 
+void CMonsterMaker :: XenmakerEffect() {
+	CMonsterMaker* xen = this;
+	
+	if (!m_xenSpriteIdx || !m_xenBeamSpriteIdx) {
+		//ALERT(at_console, "xenmaker sprites not precached");
+		return;
+	}
+
+	if (m_xenmakerTemplate) {
+		// searching every spawn so that entity order doesn't matter during map init
+		edict_t* ent = FIND_ENTITY_BY_TARGETNAME(NULL, STRING(m_xenmakerTemplate));
+		if (!FNullEnt(ent) && !strcmp(STRING(ent->v.classname), "env_xenmaker")) {
+			xen = (CMonsterMaker*)GET_PRIVATE(ent);
+		}
+	}
+
+	Vector position = pev->origin;
+	position.z += (pev->absmax.z - pev->absmin.z) * 0.5f;
+
+	const int effectDuration = 10;
+
+	// sven uses a special MSG_TE_CUSTOM message for the effect, which can
+	// mostly be recreated with existing temporary entity effects.
+	// The difference is that the sprite is killed after a set amount of time,
+	// even if the animation hasn't finished, and the framerate can be changed. 
+	// No one is going to see the sprite because they're all at 1 opacity by default,
+	// so I'll just use TE_SPRITE here.
+	MESSAGE_BEGIN(MSG_PVS, SVC_TEMPENTITY, position);
+	WRITE_BYTE(TE_SPRITE);
+	WRITE_COORD(position.x);
+	WRITE_COORD(position.y);
+	WRITE_COORD(position.z);
+	WRITE_SHORT(xen->m_xenSpriteIdx);
+	WRITE_BYTE(min(255, xen->m_flStartSpriteScale*10));
+	WRITE_BYTE(m_iStartSpriteAlpha);
+	MESSAGE_END();
+
+	MESSAGE_BEGIN(MSG_PVS, SVC_TEMPENTITY, position);
+	WRITE_BYTE(TE_DLIGHT);
+	WRITE_COORD(position.x);
+	WRITE_COORD(position.y);
+	WRITE_COORD(position.z);
+	WRITE_BYTE(xen->m_flLightRadius / 10);
+	WRITE_BYTE(xen->m_vLightColor.x);
+	WRITE_BYTE(xen->m_vLightColor.y);
+	WRITE_BYTE(xen->m_vLightColor.z);
+	WRITE_BYTE(effectDuration); // life
+	WRITE_BYTE(0); // decay rate
+	MESSAGE_END();
+
+	int createdBeams = 0;
+	TraceResult tr;
+	for (int i = 0; i < m_iBeamCount; i++) {
+		// TODO: uniform distribution
+		Vector randomDir = Vector(RANDOM_FLOAT(-1, 1), RANDOM_FLOAT(-1, 1), RANDOM_FLOAT(-1, 1));
+		Vector randomPos = position + randomDir.Normalize() * m_flBeamRadius;
+
+		TRACE_LINE(position, randomPos, ignore_monsters, NULL, &tr);
+		if (tr.flFraction < 1.0f) {
+			MESSAGE_BEGIN(MSG_PVS, SVC_TEMPENTITY, position);
+			WRITE_BYTE(TE_BEAMPOINTS);
+			WRITE_COORD(position.x);
+			WRITE_COORD(position.y);
+			WRITE_COORD(position.z);
+			WRITE_COORD(tr.vecEndPos.x);
+			WRITE_COORD(tr.vecEndPos.y);
+			WRITE_COORD(tr.vecEndPos.z);
+			WRITE_SHORT(xen->m_xenBeamSpriteIdx);
+			WRITE_BYTE(0); // frame start
+			WRITE_BYTE(0); // frame rate
+			WRITE_BYTE(effectDuration); // life
+			WRITE_BYTE(10); // width
+			WRITE_BYTE(50); // noise
+			WRITE_BYTE(xen->m_vBeamColor.x);
+			WRITE_BYTE(xen->m_vBeamColor.y);
+			WRITE_BYTE(xen->m_vBeamColor.z);
+			WRITE_BYTE(xen->m_iBeamAlpha);
+			WRITE_BYTE(0); // scroll
+			MESSAGE_END();
+
+			if (++createdBeams >= MAX_XENMAKER_BEAMS) {
+				break;
+			}
+		}
+	}
+
+	EMIT_SOUND(ENT(pev), CHAN_VOICE, XENMAKER_SOUND1, 1.0f, ATTN_NORM);
+
+	m_nextXenSound = gpGlobals->time + 1.0f;
+
+	if (!m_fActive) {
+		SetThink(&CMonsterMaker::MakerThink);
+		pev->nextthink = gpGlobals->time + 1.0f;
+	}
+}
+
 //=========================================================
 // MakerThink - creates a new monster every so often
 //=========================================================
@@ -323,9 +573,18 @@ void CMonsterMaker :: MakerThink ( void )
 {
 	pev->nextthink = gpGlobals->time + m_flDelay;
 
-	MakeMonster();
-}
+	if (m_nextXenSound && gpGlobals->time >= m_nextXenSound) {
+		m_nextXenSound = 0;
+		EMIT_SOUND(ENT(pev), CHAN_VOICE, XENMAKER_SOUND2, 1.0f, ATTN_NORM);
+	}
 
+	if (m_fActive) {
+		MakeMonster();
+	}
+	else if (!m_nextXenSound) {
+		SetThink(NULL);
+	}
+}
 
 //=========================================================
 //=========================================================
