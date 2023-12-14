b4dcacc8f57e0d43cf4968faa49a4ff6354bb3dd
wootguy <w00tguy123@gmail.com>
2023-12-14 07:49:06 -0800

    fix scientist fear crash

--- dlls/monster/CScientist.cpp
+++ dlls/monster/CScientist.cpp
@@ -476,7 +476,7 @@ void CScientist :: StartTask( Task_t *pTask )
 		{
 			Talk( 2 );
 			m_hTalkTarget = m_hEnemy;
-			if ( m_hEnemy->IsPlayer() )
+			if ( m_hEnemy && m_hEnemy->IsPlayer() )
 				PlaySentence( "SC_PLFEAR", 5, VOL_NORM, ATTN_NORM );
 			else
 				PlaySentence( "SC_FEAR", 5, VOL_NORM, ATTN_NORM );
