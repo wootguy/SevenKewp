007ccd8fae647740003aa9a1d22ca2a61b049481
wootguy <w00tguy123@gmail.com>
2024-01-07 21:12:22 -0800

    fix momentary button not fully moving its target
    the engine modulos angle values to +/-3600 and some buttons rotate far beyond that (svencoop1, toonrun2)

--- dlls/func/CMomentaryRotButton.cpp
+++ dlls/func/CMomentaryRotButton.cpp
@@ -33,6 +33,8 @@ public:
 	void	PlaySound(void);
 	void	UpdateTarget(float value);
 
+	float	GetRotProgress(Vector angles); // how close is the button to its destination angle? (0-1)
+
 	static CMomentaryRotButton* Instance(edict_t* pent) { return (CMomentaryRotButton*)GET_PRIVATE(pent); };
 	virtual int		Save(CSave& save);
 	virtual int		Restore(CRestore& restore);
@@ -45,6 +47,8 @@ public:
 	vec3_t	m_start;
 	vec3_t	m_end;
 	int		m_sounds;
+	int		m_overflows; // number of times the rotation has overflowd the 3600 engine limit
+	float	m_lastProgress;
 };
 TYPEDESCRIPTION CMomentaryRotButton::m_SaveData[] =
 {
@@ -117,18 +121,23 @@ void CMomentaryRotButton::PlaySound(void)
 	EMIT_SOUND(ENT(pev), CHAN_VOICE, (char*)STRING(pev->noise), 1, ATTN_NORM);
 }
 
+float CMomentaryRotButton::GetRotProgress(Vector angles) {
+	return (CBaseToggle::AxisDelta(pev->spawnflags, angles, m_start) + (m_overflows * 3600.0f)) / m_flMoveDistance;
+}
+
 // BUGBUG: This design causes a latentcy.  When the button is retriggered, the first impulse
 // will send the target in the wrong direction because the parameter is calculated based on the
 // current, not future position.
 void CMomentaryRotButton::Use(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value)
 {
-	pev->ideal_yaw = CBaseToggle::AxisDelta(pev->spawnflags, pev->angles, m_start) / m_flMoveDistance;
+	pev->ideal_yaw = GetRotProgress(pev->angles);
 
 	UpdateAllButtons(pev->ideal_yaw, 1);
 
 	// Calculate destination angle and use it to predict value, this prevents sending target in wrong direction on retriggering
 	Vector dest = pev->angles + pev->avelocity * (pev->nextthink - pev->ltime);
-	float value1 = CBaseToggle::AxisDelta(pev->spawnflags, dest, m_start) / m_flMoveDistance;
+	float value1 = GetRotProgress(dest);	
+
 	UpdateTarget(value1);
 
 }
@@ -156,6 +165,17 @@ void CMomentaryRotButton::UpdateAllButtons(float value, int start)
 			}
 		}
 	}
+
+	// detect when engine modulos the angle value, so it can be undone to calculate rotation progress
+	if (value < 1.0f && m_direction > 0 && value < m_lastProgress) {
+		m_overflows++;
+	}
+	else if (value > 0.0f && m_direction < 0 && value > m_lastProgress) {
+		m_overflows--;
+	}
+	m_lastProgress = value;
+
+	//ALERT(at_console, "ROT: %f %d %f\n", pev->angles.z, m_overflows, value);
 }
 
 void CMomentaryRotButton::UpdateSelf(float value)
@@ -164,6 +184,7 @@ void CMomentaryRotButton::UpdateSelf(float value)
 
 	if (!m_lastUsed)
 	{
+		m_lastProgress = value; // don't immediately trigger overflow
 		fplaysound = TRUE;
 		m_direction = -m_direction;
 	}
@@ -174,12 +195,25 @@ void CMomentaryRotButton::UpdateSelf(float value)
 	{
 		pev->avelocity = g_vecZero;
 		pev->angles = m_end;
+
+		for (int i = 0; i < 3; i++)
+			if (pev->angles[i] <= -3600.0f || pev->angles[i] >= 3600.0f)
+				pev->angles[i] = fmod(pev->angles[i], 3600.0f);
+
+		m_overflows = m_flMoveDistance / 3600.0f;
+
 		return;
 	}
 	else if (m_direction < 0 && value <= 0)
 	{
 		pev->avelocity = g_vecZero;
 		pev->angles = m_start;
+		m_overflows = 0;
+
+		for (int i = 0; i < 3; i++)
+			if (pev->angles[i] <= -3600.0f || pev->angles[i] >= 3600.0f)
+				pev->angles[i] = fmod(pev->angles[i], 3600.0f);
+
 		return;
 	}
 
@@ -219,6 +253,10 @@ void CMomentaryRotButton::Off(void)
 {
 	pev->avelocity = g_vecZero;
 	m_lastUsed = 0;
+
+	// don't immediately trigger an underflow
+	m_lastProgress = GetRotProgress(pev->angles);
+
 	if (FBitSet(pev->spawnflags, SF_PENDULUM_AUTO_RETURN) && m_returnSpeed > 0)
 	{
 		SetThink(&CMomentaryRotButton::Return);
@@ -231,9 +269,13 @@ void CMomentaryRotButton::Off(void)
 
 void CMomentaryRotButton::Return(void)
 {
-	float value = CBaseToggle::AxisDelta(pev->spawnflags, pev->angles, m_start) / m_flMoveDistance;
+	float value = GetRotProgress(pev->angles);
 
 	UpdateAllButtons(value, 0);	// This will end up calling UpdateSelfReturn() n times, but it still works right
+
+	// recalculate in case of underflow
+	value = GetRotProgress(pev->angles);
+
 	if (value > 0)
 		UpdateTarget(value);
 }
@@ -241,6 +283,8 @@ void CMomentaryRotButton::Return(void)
 
 void CMomentaryRotButton::UpdateSelfReturn(float value)
 {
+	m_lastUsed = 0;
+
 	if (value <= 0)
 	{
 		pev->avelocity = g_vecZero;
