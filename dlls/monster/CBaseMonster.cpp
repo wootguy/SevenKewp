a083798296e7831ad1a966d63a6486d87c25c83b
wootguy <drake.rhunter@gmail.com>
2024-02-07 23:49:51 -0800

    fix laggy movement on corpses
    the lag was horrible in rooms full of dead monsters. Corpses are nonsolid now which probably fixes an AI routing bug in addition to spontaneous player gibbing and laggy movement. Special logic is added to the crowbar so that you can still gib corpses (very important fun feature).

--- dlls/monster/CBaseMonster.cpp
+++ dlls/monster/CBaseMonster.cpp
@@ -5432,6 +5432,10 @@ void CBaseMonster::RunTask(Task_t* pTask)
 			else // !!!HACKHACK - put monster in a thin, wide bounding box until we fix the solid type/bounding volume problem
 				UTIL_SetSize(pev, Vector(pev->mins.x, pev->mins.y, pev->mins.z), Vector(pev->maxs.x, pev->maxs.y, pev->mins.z + 2));
 
+			// corpses cause laggy movement when walking over them, so make them nonsolid
+			// gibbing with crowbar is handled in the weapon code
+			pev->solid = SOLID_NOT;
+
 			if (ShouldFadeOnDeath())
 			{
 				// this monster was created by a monstermaker... fade the corpse out.
