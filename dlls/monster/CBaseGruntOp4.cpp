e805e312f8874df611339c6aa5778bd3d1fd571d
wootguy <drake.rhunter@gmail.com>
2024-03-12 05:16:45 -0700

    add fgrunt/otis sounds + fixes + tweak
    - saw grunts have less accuracy (same as mp5)
    - fix friendlies attacking you if you killed an enemy that normally spawns as a friendly
    - fix friendlies not attacking you if you killed a friendly that doesn't fight back when attacked (scientist)

--- dlls/monster/CBaseGruntOp4.cpp
+++ dlls/monster/CBaseGruntOp4.cpp
@@ -50,6 +50,220 @@ const char* CBaseGruntOp4::pDieSounds[] =
 	MOD_SND_FOLDER "fgrunt/death6.wav",
 };
 
+const char* CBaseGruntOp4::pGrenSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/incoming.wav",
+	MOD_SND_FOLDER "fgrunt/grenade.wav",
+};
+
+const char* CBaseGruntOp4::pAlertSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/alert.wav",
+	MOD_SND_FOLDER "fgrunt/down.wav",
+	MOD_SND_FOLDER "fgrunt/bogies.wav",
+	MOD_SND_FOLDER "fgrunt/hostiles.wav",
+};
+
+const char* CBaseGruntOp4::pMonsterSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/hostiles.wav",
+	MOD_SND_FOLDER "fgrunt/bogies.wav",
+};
+
+const char* CBaseGruntOp4::pCoverSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/clear.wav",
+	MOD_SND_FOLDER "fgrunt/cover.wav",
+	MOD_SND_FOLDER "fgrunt/staydown.wav",
+};
+
+const char* CBaseGruntOp4::pThrowSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/fire.wav",
+	MOD_SND_FOLDER "fgrunt/grenade.wav",
+};
+
+const char* CBaseGruntOp4::pChargeSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/go.wav",
+	MOD_SND_FOLDER "fgrunt/move.wav",
+	MOD_SND_FOLDER "fgrunt/getsome.wav",
+};
+
+const char* CBaseGruntOp4::pTauntSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/nothing.wav",
+	MOD_SND_FOLDER "fgrunt/getsome.wav",
+	MOD_SND_FOLDER "fgrunt/corners.wav",
+	MOD_SND_FOLDER "fgrunt/backup.wav",
+	MOD_SND_FOLDER "fgrunt/oneshot.wav",
+	MOD_SND_FOLDER "fgrunt/mister.wav",
+};
+
+const char* CBaseGruntOp4::pShotSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/friendly.wav",
+	MOD_SND_FOLDER "fgrunt/watchfire.wav",
+	MOD_SND_FOLDER "fgrunt/zone.wav",
+	MOD_SND_FOLDER "fgrunt/watchit.wav",
+	MOD_SND_FOLDER "fgrunt/check.wav",
+	MOD_SND_FOLDER "fgrunt/wantsome.wav",
+};
+
+const char* CBaseGruntOp4::pMadSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/ass.wav",
+	MOD_SND_FOLDER "fgrunt/mister.wav",
+};
+
+const char* CBaseGruntOp4::pKillSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/sniper.wav",
+	MOD_SND_FOLDER "fgrunt/tag.wav",
+	MOD_SND_FOLDER "fgrunt/corporal.wav",
+	MOD_SND_FOLDER "fgrunt/talking.wav",
+	MOD_SND_FOLDER "fgrunt/take.wav",
+	MOD_SND_FOLDER "fgrunt/area.wav",
+	MOD_SND_FOLDER "fgrunt/check.wav",
+	MOD_SND_FOLDER "fgrunt/wantsome.wav",
+	MOD_SND_FOLDER "fgrunt/oneshot.wav",
+};
+
+const char* CBaseGruntOp4::pAnswerSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/roger.wav",
+	MOD_SND_FOLDER "fgrunt/quiet.wav",
+	MOD_SND_FOLDER "fgrunt/no.wav",
+};
+
+const char* CBaseGruntOp4::pQuestionSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/seensquad.wav",
+	MOD_SND_FOLDER "fgrunt/current.wav",
+	MOD_SND_FOLDER "fgrunt/coverup.wav",
+	MOD_SND_FOLDER "fgrunt/chicken.wav",
+	MOD_SND_FOLDER "fgrunt/charge.wav",
+	MOD_SND_FOLDER "fgrunt/bfeeling.wav",
+};
+
+const char* CBaseGruntOp4::pIdleSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/guard.wav",
+	MOD_SND_FOLDER "fgrunt/mission.wav",
+	MOD_SND_FOLDER "fgrunt/babysitting.wav",
+	MOD_SND_FOLDER "fgrunt/coverup.wav",
+	MOD_SND_FOLDER "fgrunt/now.wav",
+	MOD_SND_FOLDER "fgrunt/short.wav",
+	MOD_SND_FOLDER "fgrunt/outof.wav",
+	MOD_SND_FOLDER "fgrunt/lost.wav",
+	MOD_SND_FOLDER "fgrunt/fubar.wav",
+	MOD_SND_FOLDER "fgrunt/dogs.wav",
+	MOD_SND_FOLDER "fgrunt/disney.wav",
+	MOD_SND_FOLDER "fgrunt/checkrecon.wav",
+	MOD_SND_FOLDER "fgrunt/seensquad.wav",
+};
+
+const char* CBaseGruntOp4::pOkSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/damage.wav",
+	MOD_SND_FOLDER "fgrunt/six.wav",
+	MOD_SND_FOLDER "fgrunt/moving.wav",
+	MOD_SND_FOLDER "fgrunt/gotit.wav",
+	MOD_SND_FOLDER "fgrunt/hellout.wav",
+	MOD_SND_FOLDER "fgrunt/sir.wav",
+	MOD_SND_FOLDER "fgrunt/right.wav",
+	MOD_SND_FOLDER "fgrunt/command.wav",
+	MOD_SND_FOLDER "fgrunt/notfail.wav",
+	MOD_SND_FOLDER "fgrunt/yes.wav",
+};
+
+const char* CBaseGruntOp4::pWaitSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/secure.wav",
+	MOD_SND_FOLDER "fgrunt/orders.wav",
+	MOD_SND_FOLDER "fgrunt/scout.wav",
+	MOD_SND_FOLDER "fgrunt/guardduty.wav",
+};
+
+const char* CBaseGruntOp4::pStopSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/post.wav",
+	MOD_SND_FOLDER "fgrunt/stay.wav",
+	MOD_SND_FOLDER "fgrunt/leave.wav",
+};
+
+const char* CBaseGruntOp4::pScaredSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/hearsome.wav",
+	MOD_SND_FOLDER "fgrunt/hear.wav",
+};
+
+const char* CBaseGruntOp4::pHelloSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/sir_01.wav",
+	MOD_SND_FOLDER "fgrunt/tosee.wav",
+	MOD_SND_FOLDER "fgrunt/hellosir.wav",
+};
+
+const char* CBaseGruntOp4::pCureSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/needmedic.wav",
+	MOD_SND_FOLDER "fgrunt/hurt.wav",
+	MOD_SND_FOLDER "fgrunt/hell.wav",
+	MOD_SND_FOLDER "fgrunt/feel.wav",
+};
+
+const char* CBaseGruntOp4::pWoundSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/imhit.wav",
+	MOD_SND_FOLDER "fgrunt/fwound.wav",
+	MOD_SND_FOLDER "fgrunt/makeit.wav",
+};
+
+const char* CBaseGruntOp4::pMortalSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/hitbad.wav",
+	MOD_SND_FOLDER "fgrunt/critical.wav",
+	MOD_SND_FOLDER "fgrunt/sdamage.wav",
+};
+
+const char* CBaseGruntOp4::pAttackSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/flank.wav",
+	MOD_SND_FOLDER "fgrunt/suppressing.wav",
+	MOD_SND_FOLDER "fgrunt/sweep.wav",
+	MOD_SND_FOLDER "fgrunt/wantsome.wav",
+	MOD_SND_FOLDER "fgrunt/freaks.wav",
+	MOD_SND_FOLDER "fgrunt/recon.wav",
+	MOD_SND_FOLDER "fgrunt/rapidfire.wav",
+	MOD_SND_FOLDER "fgrunt/marines.wav",
+	MOD_SND_FOLDER "fgrunt/moveup.wav",
+	MOD_SND_FOLDER "fgrunt/go.wav",
+	MOD_SND_FOLDER "fgrunt/covering.wav",
+	MOD_SND_FOLDER "fgrunt/ass.wav",
+};
+
+const char* CBaseGruntOp4::pPokSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/hell.wav",
+	MOD_SND_FOLDER "fgrunt/sir_01.wav",
+	MOD_SND_FOLDER "fgrunt/hellosir.wav",
+	MOD_SND_FOLDER "fgrunt/corporal_01.wav",
+};
+
+const char* CBaseGruntOp4::pCheckSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/check.wav",
+	MOD_SND_FOLDER "fgrunt/zone.wav",
+};
+
+const char* CBaseGruntOp4::pClearSounds[] =
+{
+	MOD_SND_FOLDER "fgrunt/clear.wav",
+	MOD_SND_FOLDER "fgrunt/area.wav",
+};
+
+
 void CBaseGruntOp4::InitAiFlags(void) {
 	canBeMadAtPlayer = true;
 	waitForEnemyFire = true;
@@ -59,7 +273,8 @@ void CBaseGruntOp4::InitAiFlags(void) {
 }
 
 void CBaseGruntOp4::PlaySentenceSound(int sentenceType) {
-	SENTENCEG_PlayRndSz(ENT(pev), pGruntSentences[sentenceType], ALLY_GRUNT_SENTENCE_VOLUME, GRUNT_ATTN, 0, m_voicePitch);
+	PlaySentence(pGruntSentences[sentenceType], 3.0f, ALLY_GRUNT_SENTENCE_VOLUME, GRUNT_ATTN);
+	//SENTENCEG_PlayRndSz(ENT(pev), pGruntSentences[sentenceType], ALLY_GRUNT_SENTENCE_VOLUME, GRUNT_ATTN, 0, m_voicePitch);
 }
 
 int	CBaseGruntOp4::Classify(void)
@@ -105,15 +320,15 @@ void CBaseGruntOp4::IdleSound(void)
 			switch (RANDOM_LONG(0, 2))
 			{
 			case 0: // check in
-				SENTENCEG_PlayRndSz(ENT(pev), "FG_CHECK", ALLY_GRUNT_SENTENCE_VOLUME, ATTN_NORM, 0, m_voicePitch);
+				PlaySentence("FG_CHECK", 3.0f, ALLY_GRUNT_SENTENCE_VOLUME, GRUNT_ATTN);
 				g_fAllyQuestion = 1;
 				break;
 			case 1: // question
-				SENTENCEG_PlayRndSz(ENT(pev), "FG_QUEST", ALLY_GRUNT_SENTENCE_VOLUME, ATTN_NORM, 0, m_voicePitch);
+				PlaySentence("FG_QUESTION", 3.0f, ALLY_GRUNT_SENTENCE_VOLUME, GRUNT_ATTN);
 				g_fAllyQuestion = 2;
 				break;
 			case 2: // statement
-				SENTENCEG_PlayRndSz(ENT(pev), "FG_IDLE", ALLY_GRUNT_SENTENCE_VOLUME, ATTN_NORM, 0, m_voicePitch);
+				PlaySentence("FG_IDLE", 3.0f, ALLY_GRUNT_SENTENCE_VOLUME, GRUNT_ATTN);
 				break;
 			}
 		}
@@ -122,10 +337,10 @@ void CBaseGruntOp4::IdleSound(void)
 			switch (g_fAllyQuestion)
 			{
 			case 1: // check in
-				SENTENCEG_PlayRndSz(ENT(pev), "FG_CLEAR", ALLY_GRUNT_SENTENCE_VOLUME, ATTN_NORM, 0, m_voicePitch);
+				PlaySentence("FG_CLEAR", 3.0f, ALLY_GRUNT_SENTENCE_VOLUME, GRUNT_ATTN);
 				break;
 			case 2: // question 
-				SENTENCEG_PlayRndSz(ENT(pev), "FG_ANSWER", ALLY_GRUNT_SENTENCE_VOLUME, ATTN_NORM, 0, m_voicePitch);
+				PlaySentence("FG_ANSWER", 3.0f, ALLY_GRUNT_SENTENCE_VOLUME, GRUNT_ATTN);
 				break;
 			}
 			g_fAllyQuestion = 0;
@@ -180,10 +395,132 @@ void CBaseGruntOp4::Precache()
 
 	PRECACHE_SOUND_ARRAY(pDieSounds);
 	PRECACHE_SOUND_ARRAY(pPainSounds);
+	PRECACHE_SOUND_ARRAY(pGrenSounds);
+	PRECACHE_SOUND_ARRAY(pAlertSounds);
+	PRECACHE_SOUND_ARRAY(pMonsterSounds);
+	PRECACHE_SOUND_ARRAY(pCoverSounds);
+	PRECACHE_SOUND_ARRAY(pThrowSounds);
+	PRECACHE_SOUND_ARRAY(pChargeSounds);
+	PRECACHE_SOUND_ARRAY(pTauntSounds);
+	PRECACHE_SOUND_ARRAY(pShotSounds);
+	PRECACHE_SOUND_ARRAY(pMadSounds);
+	PRECACHE_SOUND_ARRAY(pKillSounds);
+	PRECACHE_SOUND_ARRAY(pAnswerSounds);
+	PRECACHE_SOUND_ARRAY(pQuestionSounds);
+	PRECACHE_SOUND_ARRAY(pIdleSounds);
+	PRECACHE_SOUND_ARRAY(pOkSounds);
+	PRECACHE_SOUND_ARRAY(pWaitSounds);
+	PRECACHE_SOUND_ARRAY(pStopSounds);
+	PRECACHE_SOUND_ARRAY(pScaredSounds);
+	PRECACHE_SOUND_ARRAY(pHelloSounds);
+	PRECACHE_SOUND_ARRAY(pCureSounds);
+	PRECACHE_SOUND_ARRAY(pWoundSounds);
+	PRECACHE_SOUND_ARRAY(pMortalSounds);
+	PRECACHE_SOUND_ARRAY(pAttackSounds);
+	PRECACHE_SOUND_ARRAY(pPokSounds);
+	PRECACHE_SOUND_ARRAY(pCheckSounds);
+	PRECACHE_SOUND_ARRAY(pClearSounds);
 
-	PRECACHE_SOUND("fgrunt/medic.wav");
+	PRECACHE_SOUND(MOD_SND_FOLDER "fgrunt/medic.wav");
 
 	TalkInit();
+}
+
+void CBaseGruntOp4::PlaySentence(const char* pszSentence, float duration, float volume, float attenuation)
+{
+	if (!pszSentence)
+		return;
+
+	Talk(duration);
+
+	CTalkSquadMonster::g_talkWaitTime = gpGlobals->time + duration + 2.0;
+
+	const char* sample = "";
+
+	if (!strcmp(pszSentence, "FG_GREN")) {
+		sample = RANDOM_SOUND_ARRAY(pGrenSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_ALERT")) {
+		sample = RANDOM_SOUND_ARRAY(pAlertSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_MONSTER")) {
+		sample = RANDOM_SOUND_ARRAY(pMonsterSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_COVER")) {
+		sample = RANDOM_SOUND_ARRAY(pCoverSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_THROW")) {
+		sample = RANDOM_SOUND_ARRAY(pThrowSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_CHARGE")) {
+		sample = RANDOM_SOUND_ARRAY(pChargeSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_TAUNT")) {
+		sample = RANDOM_SOUND_ARRAY(pTauntSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_SHOT")) {
+		sample = RANDOM_SOUND_ARRAY(pShotSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_MAD")) {
+		sample = RANDOM_SOUND_ARRAY(pMadSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_KILL")) {
+		sample = RANDOM_SOUND_ARRAY(pKillSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_ANSWER")) {
+		sample = RANDOM_SOUND_ARRAY(pAnswerSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_QUESTION")) {
+		sample = RANDOM_SOUND_ARRAY(pQuestionSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_IDLE")) {
+		sample = RANDOM_SOUND_ARRAY(pIdleSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_OK")) {
+		sample = RANDOM_SOUND_ARRAY(pOkSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_WAIT")) {
+		sample = RANDOM_SOUND_ARRAY(pWaitSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_STOP")) {
+		sample = RANDOM_SOUND_ARRAY(pStopSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_SCARED")) {
+		sample = RANDOM_SOUND_ARRAY(pScaredSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_HELLO")) {
+		sample = RANDOM_SOUND_ARRAY(pHelloSounds);
+	}
+	else if (!strcmp(pszSentence, "!FG_CUREA") || !strcmp(pszSentence, "!FG_CUREB") || !strcmp(pszSentence, "!FG_CUREC")) {
+		sample = RANDOM_SOUND_ARRAY(pCureSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_WOUND")) {
+		sample = RANDOM_SOUND_ARRAY(pWoundSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_MORTAL")) {
+		sample = RANDOM_SOUND_ARRAY(pMortalSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_ATTACK")) {
+		sample = RANDOM_SOUND_ARRAY(pAttackSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_POK")) {
+		sample = RANDOM_SOUND_ARRAY(pPokSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_CHECK")) {
+		sample = RANDOM_SOUND_ARRAY(pCheckSounds);
+	}
+	else if (!strcmp(pszSentence, "FG_CLEAR")) {
+		sample = RANDOM_SOUND_ARRAY(pClearSounds);
+	}
+	else {
+		ALERT(at_console, "Invalid sentence: %s\n", pszSentence);
+		return;
+	}
+
+	EMIT_SOUND_DYN(edict(), CHAN_VOICE, sample, volume, attenuation, 0, GetVoicePitch());
+
+	// If you say anything, don't greet the player - you may have already spoken to them
+	SetBits(m_bitsSaid, bit_saidHelloPlayer);
 }
 
 void CBaseGruntOp4::TalkInit()
@@ -193,7 +530,7 @@ void CBaseGruntOp4::TalkInit()
 	m_szGrp[TLK_ANSWER] = "FG_ANSWER";
 	m_szGrp[TLK_QUESTION] = "FG_QUESTION";
 	m_szGrp[TLK_IDLE] = "FG_IDLE";
-	m_szGrp[TLK_STARE] = "FG_STARE";
+	m_szGrp[TLK_STARE] = NULL; // "FG_STARE";
 	m_szGrp[TLK_USE] = "FG_OK";
 	m_szGrp[TLK_UNUSE] = "FG_WAIT";
 	m_szGrp[TLK_STOP] = "FG_STOP";
@@ -207,9 +544,9 @@ void CBaseGruntOp4::TalkInit()
 
 	m_szGrp[TLK_PHELLO] = NULL;	//"BA_PHELLO";		// UNDONE
 	m_szGrp[TLK_PIDLE] = NULL;	//"BA_PIDLE";			// UNDONE
-	m_szGrp[TLK_PQUESTION] = "FG_PQUEST";		// UNDONE
+	m_szGrp[TLK_PQUESTION] = NULL;// "FG_PQUEST";		// UNDONE
 
-	m_szGrp[TLK_SMELL] = "FG_SMELL";
+	m_szGrp[TLK_SMELL] = NULL; // "FG_SMELL";
 
 	m_szGrp[TLK_WOUND] = "FG_WOUND";
 	m_szGrp[TLK_MORTAL] = "FG_MORTAL";
