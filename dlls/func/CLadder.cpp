1ba7afee651a5137cec933e1f35a0d907fe28f9b
wootguy <drake.rhunter@gmail.com>
2024-04-11 23:18:17 -0700

    allow toggling ladders + add "start off" flag

--- dlls/func/CLadder.cpp
+++ dlls/func/CLadder.cpp
@@ -6,6 +6,8 @@
 #include "gamerules.h"
 #include "CBaseTrigger.h"
 
+#define SF_LADDER_START_OFF 1
+
 class CLadder : public CBaseTrigger
 {
 public:
@@ -13,6 +15,7 @@ public:
 	void KeyValue(KeyValueData* pkvd);
 	void Spawn(void);
 	void Precache(void);
+	void Toggle(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value);
 };
 LINK_ENTITY_TO_CLASS(func_ladder, CLadder);
 
@@ -20,6 +23,12 @@ LINK_ENTITY_TO_CLASS(func_ladder, CLadder);
 void CLadder::KeyValue(KeyValueData* pkvd)
 {
 	CBaseTrigger::KeyValue(pkvd);
+}
+
+void CLadder::Toggle(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value) {
+
+	pev->skin = pev->skin ? 0 : CONTENTS_LADDER;
+	UTIL_SetOrigin(pev, pev->origin);
 }
 
 
@@ -30,13 +39,16 @@ void CLadder::Precache(void)
 {
 	// Do all of this in here because we need to 'convert' old saved games
 	pev->solid = SOLID_NOT;
-	pev->skin = CONTENTS_LADDER;
+	pev->skin = (pev->spawnflags & SF_LADDER_START_OFF) ? 0 : CONTENTS_LADDER;
+
 	if (CVAR_GET_FLOAT("showtriggers") == 0)
 	{
 		pev->rendermode = kRenderTransTexture;
 		pev->renderamt = 0;
 	}
 	pev->effects &= ~EF_NODRAW;
+
+	SetUse(&CLadder::Toggle);
 
 	PRECACHE_SOUND("player/pl_ladder1.wav");
 	PRECACHE_SOUND("player/pl_ladder2.wav");
@@ -51,5 +63,7 @@ void CLadder::Spawn(void)
 
 	SET_MODEL(ENT(pev), STRING(pev->model));    // set size and link into world
 	pev->movetype = MOVETYPE_PUSH;
+
+	UTIL_SetOrigin(pev, pev->origin);
 }
 
