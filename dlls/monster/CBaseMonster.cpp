717c15bd73687daf452aa4c6c5e0df053526e24e
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-10-05 23:22:27 -0700

    nerf middle eastern warfare tactics

--- dlls/monster/CBaseMonster.cpp
+++ dlls/monster/CBaseMonster.cpp
@@ -4675,9 +4675,11 @@ void CBaseMonster::GiveScorePoints(entvars_t* pevAttacker, float damageDealt) {
 		const float MONSTER_POINTS_PER_HP = 0.01f; // how many points to give per hitpoint of damage dealt
 		// float damageAmt = damageDealt > 0 ? V_min(damageDealt, pev->health) : V_min(damageDealt, pev->max_health - pev->health);
 		// bool isFriendly = attackMon->IRelationship(this) == R_AL;
-		pevAttacker->frags += damageDealt * 1 * MONSTER_POINTS_PER_HP;
+		if (attackMon != pevAttacker) {
+			pevAttacker->frags += damageDealt * 1 * MONSTER_POINTS_PER_HP;
 
-		LogPlayerDamage(pevAttacker, damageDealt);
+			LogPlayerDamage(pevAttacker, damageDealt);
+		}
 	}
 }
 
