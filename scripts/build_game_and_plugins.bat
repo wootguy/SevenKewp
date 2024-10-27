27aab7cdb546d0e230854c84164fee3332bf719a
wootguy <w00tguy123@gmail.com>
2024-10-26 21:16:55 -0700

    add script to build projects in plugins/ folder
    faster than compiling plugins from their independent repos

---
+++ scripts/build_game_and_plugins.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+
+:: run from the location of this script
+cd %~dp0
+cd ..
+
+mkdir build
+cd build
+cmake -A Win32 -DBUILD_SERVER_ONLY=ON -DBUILD_PLUGINS=ON ..
+cmake --build . --config Release
+
+echo.
+pause
---
+++ scripts/build_game_and_plugins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# run from the location of this script
+cd "$(dirname "$0")"
+cd ..
+
+rm -rf build
+mkdir build
+cd build
+cmake -DBUILD_SERVER_ONLY=ON -DBUILD_PLUGINS=ON ..
+cmake --build . --config Release
