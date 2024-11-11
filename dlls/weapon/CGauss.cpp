081073146cb133c7511c29c8a09cbb3ae9b07342
wootguy <drake.rhunter@gmail.com>
2024-11-10 22:08:23 -0800

    client and listen server fixes/testing
    - fixed scoreboard not showing team info
    - valve_addon/ is searched for game files
    
    Place the server lib in valve_addon/dlls/server.dll. Launch with "-dll dlls/server.dll". The server lib must be called "server" or else plugins won't load. Blood wasn't showing for me at first because violence_ablood/violence_hblood were 0 for some reason.

--- dlls/weapon/CGauss.cpp
+++ dlls/weapon/CGauss.cpp
@@ -303,7 +303,6 @@ void CGauss::SecondaryAttack()
 
 		if (pitch != m_lastPitch && (m_lastPitch % 4 == 0 || pitch == 250) || m_iSoundState != SND_CHANGE_PITCH) {
 			PLAYBACK_EVENT_FULL(FEV_NOTHOST, m_pPlayer->edict(), m_usGaussSpin, 0.0, (float*)&g_vecZero, (float*)&g_vecZero, 0.0, 0.0, pitch, 0, (m_iSoundState == SND_CHANGE_PITCH) ? 1 : 0, 0);
-			ALERT(at_console, "SEND EVENT: %d\n", pitch);
 		}
 
 		m_lastPitch = pitch;
