861d3aeec8082c7f2b2ae81e6bce12c51da1e0b0
wootguy <w00tguy123@gmail.com>
2026-02-27 19:15:59 -0800

    whitelist mp_max_pvs_corpses

--- dlls/game/gamerules.cpp
+++ dlls/game/gamerules.cpp
@@ -202,6 +202,7 @@ void execMapCfg(const char* cfgPath, StringSet& openedCfgs) {
 		"mp_mergemodels",
 		"mp_skill_allow",
 		"mp_bigmap",
+		"mp_max_pvs_corpses",
 		"killnpc",
 		"mp_npckill",
 		"mp_startflashlight",
